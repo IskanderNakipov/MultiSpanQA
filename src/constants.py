57a89c47797bff701672ec08f869192494437030
inakipov <inakipov@griddynamics.com>
2023-02-22 23:21:39 +0400

    fix

--- src/constants.py
+++ src/constants.py
@@ -1,2 +1,27 @@
-STRUCTURE_LIST = ['Complex', 'Conjunction', 'Non-Redundant', 'Redundant', 'Share', '']
+QUESTIONS = {
+    'rank': "Какое звание у {}?",
+    'dolls': "Какая должность у {}?",
+    'medals': "Какие награды получил {}?",
+    'age': "Сколько лет было {}?",
+    'death_date': "Когда погиб {}?",
+    'death_place': "Где погиб {}?",
+    'birth_date': "Когда родился  {}?",
+    'birth_place': "Где родился  {}?",
+    'burial_date': "Когда было прощание с {}?",
+    'burial_place': "Где было прощание с  {}?",
+}
+
+STRUCTURE_LIST = [
+    'rank',
+    'dolls',
+    'medals',
+    'age',
+    'death_date',
+    'death_place',
+    'birth_date',
+    'birth_place',
+    'burial_date',
+    'burial_place'
+]
+
 STRUCTURE_TO_ID = {l: i for i, l in enumerate(STRUCTURE_LIST)}
